854194cd403e03f32519e2eb4e62d444af8f6e1d
Chris Rohlfs <seeken@gmail.com>
2022-10-16 22:19:36 -0600

    add notes

--- lib/listable_components_petal/view_selector.ex
+++ lib/listable_components_petal/view_selector.ex
@@ -8,17 +8,22 @@ defmodule ListableComponentsPetal.ViewSelector do
     assigns = assign(assigns, columns: Map.values(assigns.listable.config.columns))
     ~H"""
       <div>
+      replace accordion with somethign that pops out from side? Or tabs?
         <.accordion>
           <:item heading="View Options">
+            Pick Aggregate or Detail View or Plugged-in view
             <div :for={c <- @columns}>
               <%= c.colid %>
             </div>
           </:item>
           <:item heading="Filter Options">
-            Filter OPTS
+            <div :for={c <- @columns}>
+              <%= c.colid %>
+            </div>
           </:item>
           <:item heading="Export Options">
-            Export OPTS
+            Export a: spreadsheet, JSON, XML, txt/csv, the SQL query (if permitted), a JSON file containing this config,
+            a link with query string to bookmark this config,
           </:item>
         </.accordion>
       </div>
