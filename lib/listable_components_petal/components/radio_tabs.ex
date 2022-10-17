f16e30533062845029e333249ee4d939f0dfce0c
Chris Rohlfs <seeken@gmail.com>
2022-10-17 13:56:32 -0600

    view selection / radio tabls comp

--- lib/listable_components_petal/components/radio_tabs.ex
+++ lib/listable_components_petal/components/radio_tabs.ex
@@ -3,5 +3,47 @@ defmodule ListableComponentsPetal.Components.RadioTabs do
     Given a set of radio buttons and containers, display the container indicated by the selected radio button
 
   """
+  use Phoenix.LiveComponent
+
+  attr :fieldname, :string, required: false
+  attr :view_sel, :string, required: false
+
+
+  # slot :section, required: true do
+  #   attr :id, :string
+  #   attr :label, :string
+  # end
+
+  def render(assigns) do
+
+    ~H"""
+      <div>
+        <div :for={s <- @section}>
+          <label>
+            <input
+              type="radio"
+              name="@fieldname"
+              value={s.id}
+              checked={@view_sel == s.id}
+              phx-click={:view_set}
+              phx-target={@myself}/>
+              <%= s.label %>
+          </label>
+          <div :if={@view_sel == s.id}>
+            <%= render_slot(s) %>
+          </div>
+        </div>
+      </div>
+    """
+
+  end
+
+  def handle_event("view_set", params, socket) do
+    send self(), {:view_set, params["value"]}
+    IO.inspect(params)
+    IO.inspect(socket)
+    {:noreply, socket}
+  end
+
 
 end
--- lib/listable_components_petal/view_selector.ex
+++ lib/listable_components_petal/view_selector.ex
@@ -2,52 +2,45 @@ defmodule ListableComponentsPetal.ViewSelector do
   use Phoenix.LiveComponent
 
   #use Phoenix.Component
-  use PetalComponents
+  #use PetalComponents
+  import ListableComponentsPetal.Components.RadioTabs
 
   def render(assigns) do
     assigns = assign(assigns, columns: Map.values(assigns.listable.config.columns))
     ~H"""
       <div>
-        <.container max-width="full">
-          <.container max-width="full">
-            <label>
-              <input type="radio" name={:view_type} value="summary"/>
-                Aggregate View
-            </label>
-            <.container max-width="full">
+        <.live_component
+          module={ListableComponentsPetal.Components.RadioTabs}
+          id="view_sel"
+          fieldname="viewsel"
+          view_sel={@view_sel}
+        >
+          <:section id="aggregate" label="Aggregate View">
+            AGG
+          </:section>
+
+          <:section id="detail" label="Detail View">
+            DET
+          </:section>
+        </.live_component>
+
               Group By:
                 Display a list of columns which can be group-by,
                 each time a new seletion is made allow them to add another.
                 Allow them to reorder
-            </.container>
-
-            <.container max-width="full">
               Aggregates:
                 Display a list of available and selected columns, and when selected
                 allow user to pick an aggregate. Allow them to reorder
-            </.container>
-          </.container>
 
-          <.container max-width="full">
-            <label>
-              <input type="radio" name={:view_type} value="detail"/>
-              Detail View
-            </label>
-            <.container max-width="full">
               Columns:
                 Display a list of available and selected columns, and when selected
                 allow user to pick formatting info. Allow them to reorder
-            </.container>
-            <.container max-width="full">
               Ordering:
                 Similar to group-by
-            </.container>
 
-          </.container>
-          ===Custom View HERE===
-        </.container>
       </div>
     """
   end
+
 
 end
