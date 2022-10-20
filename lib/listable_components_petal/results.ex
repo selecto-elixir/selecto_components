b7e85d33f0316700d455e4bb6dae5e5700b4d33a
Chris Rohlfs <seeken@gmail.com>
2022-10-20 15:40:18 -0600

    restyle res

--- lib/listable_components_petal/results.ex
+++ lib/listable_components_petal/results.ex
@@ -13,20 +13,18 @@ defmodule ListableComponentsPetal.Results do
 
     ~H"""
     <div>
-      <.container max-width="full">
-        <.table>
-          <.tr>
-            <.th :for={r <- @aliases}>
-              <%= r %>
-            </.th>
-          </.tr>
-          <.tr :for={r <- @results}>
-            <.td :for={c <- @aliases}>
-              <%= r[c] %>
-            </.td>
-          </.tr>
-        </.table>
-      </.container>
+      <table class="min-w-full overflow-hidden divide-y ring-1 ring-gray-200 dark:ring-0 divide-gray-200 rounded-sm table-auto dark:divide-y-0 dark:divide-gray-800 sm:rounded">
+        <tr>
+          <th :for={r <- @aliases} class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
+            <%= r %>
+          </th>
+        </tr>
+        <tr :for={r <- @results} class="border-b dark:border-gray-700 bg-white even:bg-white dark:bg-gray-700 dark:even:bg-gray-800 last:border-none">
+          <td :for={c <- @aliases} class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">
+            <%= r[c] %>
+          </td>
+        </tr>
+      </table>
     </div>
     """
   end
