c7b727d3a86e3ba6c0f5d6cfb5aad34d67f83a9f
Chris Rohlfs <seeken@gmail.com>
2022-10-27 16:29:12 -0600

    formatting function in agg

--- lib/listable_components_tailwind/components/aggregate_table.ex
+++ lib/listable_components_tailwind/components/aggregate_table.ex
@@ -7,30 +7,72 @@ defmodule ListableComponentsTailwind.Components.AggregateTable do
   def render(assigns) do
     {results, aliases} = Listable.execute(assigns.listable)
 
+    IO.inspect(results)
     group_by = assigns.listable.set.group_by
     aggregates = assigns.listable.set.selected -- group_by
-    assigns = assign(assigns, results: results, aliases: aliases, group_by: group_by, aggregate: aggregates)
+
+    group_by = Enum.map( group_by,
+       fn
+        {:extract, f, fmt} = g ->
+          {:group_by, g, assigns.listable.config.columns[f]}
+        {a, f} = g ->
+          {:group_by, g, assigns.listable.config.columns[f]}
+      end)
+
+    aggregates = Enum.map( aggregates, fn
+      {:extract, f, fmt} = agg ->
+        {:agg, agg, assigns.listable.config.columns[f]}
+      {a, f} = agg->
+        {:agg, agg, assigns.listable.config.columns[f]}
+    end)
+
+    fmap = Enum.zip(aliases, group_by ++ aggregates ) |> Enum.into(%{})
+
+    assigns = assign(assigns, results: results, aliases: aliases, group_by: group_by, aggregate: aggregates, fmap: fmap)
+
+
 
     ~H"""
     <div>
       <table class="min-w-full overflow-hidden divide-y ring-1 ring-gray-200 dark:ring-0 divide-gray-200 rounded-sm table-auto dark:divide-y-0 dark:divide-gray-800 sm:rounded">
         <tr>
-          <th :for={g <- @group_by} class="font-bold px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
+
+          <th :for={{:group_by, g, def} <- @group_by} class="font-bold px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
             <%= case g do %>
             <%= {:extract, f, fmt} -> %>
+              <%= fmt %>: <%= def.name %>
+            <% {a, f} -> %>
+              <%= a %>: <%= f %>
+            <% f -> %>
               <%= inspect(f) %>
-            <%= f -> %>
-              <%= inspect(f)  %>
             <% end %>
-
           </th>
+
           <th :for={r <- @aggregate} class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
-            <%= inspect(r) %>
+            <%= case r do %>
+            <% {:agg, {a, f}, def} -> %>
+              <%= a %>: <%= f %>
+            <% f -> %>
+              <%= inspect(f) %>
+            <% end %>
           </th>
         </tr>
+
         <tr :for={r <- @results} class="border-b dark:border-gray-700 bg-white even:bg-white dark:bg-gray-700 dark:even:bg-gray-800 last:border-none">
+          <% IO.inspect(r) %>
           <td :for={c <- @aliases} class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">
-            <%= r[c] %>
+            <%= with def <- @fmap[c] do %>
+              <% IO.inspect(def) %>
+              <%= case def do %>
+                <% {:group_by, g, def} -> %>
+                  GROUP <%= r[c] %>
+
+                <% {:agg, {func, _field}, %{format: fmt_fun} = def} -> %>
+                  FMT AGG <%= fmt_fun.(r[c]) %>
+                <% _ -> %>
+                  AGG <%= r[c] %>
+              <% end %>
+            <% end %>
           </td>
         </tr>
       </table>
