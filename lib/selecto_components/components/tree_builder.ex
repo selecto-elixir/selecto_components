0fcfca95135d8be40d54314f8ad8a9be214ce97e
Chris Rohlfs <seeken@gmail.com>
2022-10-28 21:11:16 -0600

    style

--- lib/selecto_components/components/tree_builder.ex
+++ lib/selecto_components/components/tree_builder.ex
@@ -8,20 +8,20 @@ defmodule SelectoComponents.Components.TreeBuilder do
 
   def render(assigns) do
     ~H"""
-      <div>
-        <div phx-hook="PushEventHook" id="relay" class="grid grid-cols-2 gap-1">
+      <div class="">
+        <div phx-hook="PushEventHook" id="relay" class="grid grid-cols-2 gap-1 max-h-70">
 
           <div>Available Filter Columns</div>
           <div>Build Area. All top level filters are AND'd together and AND'd with the required filters from the domain.</div>
 
-          <div class="grid grid-cols-1 gap-1 border-solid border rounded-md border-grey dark:border-black max-h-120 overflow-auto p-1">
+          <div class="grid grid-cols-1 gap-1 border-solid border rounded-md border-grey dark:border-black max-h-70 overflow-auto p-1">
 
             <div :for={{id, name} <- @available}>
               <div draggable="true" x-on:drag=" dragging = event.srcElement.id; " id={id}><%= name %></div>
             </div>
 
           </div>
-          <div class="grid grid-cols-1 gap-1 border-solid border rounded-md border-grey dark:border-black max-h-120 overflow-auto p-1">
+          <div class="grid grid-cols-1 gap-1 border-solid border rounded-md border-grey dark:border-black max-h-70 overflow-auto p-1">
             <%= render_area(%{ available: @available, filters: @filters, section: "filters[main]", conjunction: 'AND', filter_form: @filter_form }) %>
 
           </div>
