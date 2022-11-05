ad6c97f4e067714fdb482bf50605f36404cd4eda
Chris Rohlfs <seeken@gmail.com>
2022-11-04 23:26:17 -0600

    update prep for hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule SelectoComponents.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: "LiveView Components for Selecto",
+      package: package(),
       deps: deps()
     ]
   end
@@ -29,6 +31,14 @@ defmodule SelectoComponents.MixProject do
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/seeken/selecto_components"},
+      source_url: "https://github.com/seeken/selecto_components"
     ]
   end
 end
