44e9a84a655668f5daf176c2e7b692bd8c276a1c
Chris Rohlfs <seeken@gmail.com>
2022-11-07 16:46:39 -0700

    support link cols

--- lib/selecto_components/components/detail_table.ex
+++ lib/selecto_components/components/detail_table.ex
@@ -5,11 +5,13 @@ defmodule SelectoComponents.Components.DetailTable do
   """
 
   use Phoenix.LiveComponent
+  use Phoenix.HTML
+
 
   def render(assigns) do
     {results, aliases} = Selecto.execute(assigns.selecto)
 
-    selected = assigns.selecto.set.selected
+    selected = assigns.selecto.set.columns
 
     selected =
       Enum.map(selected, fn
@@ -20,19 +22,22 @@ defmodule SelectoComponents.Components.DetailTable do
           {f, assigns.selecto.config.columns[f]}
       end)
 
-    fmap = Enum.zip(aliases, selected) |> Enum.into(%{})
+    fmap = Enum.zip(aliases, selected) |> Enum.into(%{}) |> IO.inspect
 
 
     page = assigns.page;
     per_page = assigns.per_page
-    IO.inspect(per_page)
 
     show_start = page * per_page
     show_end = show_start + per_page
 
     page_count = Float.ceil(Enum.count(results) / per_page)
 
-    assigns = assign(assigns, fmap: fmap, results: results, aliases: aliases, max_pages: page_count)
+
+    ### Use Selecto columns rather than aliases because a column can lead to more than one selection...
+
+
+    assigns = assign(assigns, fmap: fmap, results: results, columns: assigns.selecto.set.columns, max_pages: page_count)
 
     ~H"""
     <div>
@@ -46,23 +51,31 @@ defmodule SelectoComponents.Components.DetailTable do
       <table class="min-w-full overflow-hidden divide-y ring-1 ring-gray-200 dark:ring-0 divide-gray-200 rounded-sm table-auto dark:divide-y-0 dark:divide-gray-800 sm:rounded">
         <tr>
           <th class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">#</th>
-          <th :for={r <- @aliases} class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
-            <%= r %>
+          <th :for={{_uuid, r} <- @columns} class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
+            <%= r["field"] %>
           </th>
         </tr>
         <tr :for={{r, i} <- Enum.slice(Enum.with_index(@results), show_start, per_page)} class="border-b dark:border-gray-700 bg-white even:bg-white dark:bg-gray-700 dark:even:bg-gray-800 last:border-none">
           <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">
             <%= i + 1 %>
           </td>
-          <td :for={c <- @aliases} class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">
-            <%= with {sel, def} <- @fmap[c] do %>
-              <%= case def do %>
-                <% %{link: fmt_fun} = def when is_function(fmt_fun) -> %>
+          <td :for={{_uuid, c} <- @columns} class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">
 
-                <% %{format: fmt_fun} = def when is_function(fmt_fun) -> %>
-                  <%= fmt_fun.(r[c]) %>
+            <%= with def <- @selecto.config.columns[c["field"]] do %>
+
+              <%= case def do %>
+                <%= %{format: :link} = def -> %>
+
+                  <%= with {href, txt} <- def.link_parts.(r)  do %>
+
+                    <.link href={href}>
+                      <%= txt %>
+                    </.link>
+
+                  <% end %>
+
                 <% _ -> %>
-                  <%= r[c] %>
+                  <%= r[c["field"]] %>
               <%= end %>
             <%= end %>
           </td>
