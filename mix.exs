818bd76e3024bdb6d688c3d1756258c435304cb9
Chris Rohlfs <seeken@gmail.com>
2022-12-08 17:44:22 -0700

    upd version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SelectoComponents.MixProject do
   def project do
     [
       app: :selecto_components,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: "LiveView Components for Selecto",
