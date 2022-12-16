24664fad9db33ed3de77b5954c436d01506f1031
Chris Rohlfs <seeken@gmail.com>
2022-12-15 21:12:40 -0700

    move date proc to selecto

--- lib/selecto_components/helpers/filters.ex
+++ lib/selecto_components/helpers/filters.ex
@@ -38,53 +38,8 @@ defmodule SelectoComponents.Helpers.Filters do
     end
   end
 
-  defp expand_date(%{"year"=>year, "month"=>"", "day"=>""}) do
-    start = Timex.to_datetime({{String.to_integer(year), 1, 1},{0,0,0}},  "Etc/UTC")
-    stop = Timex.end_of_year(start)
-    {start, stop}
-  end
-
-  defp expand_date(%{"year"=>year, "month"=>month, "day"=>""}) do
-    start = Timex.to_datetime({{String.to_integer(year), String.to_integer(month), 1},{0,0,0}},  "Etc/UTC")
-    stop = Timex.end_of_month(start)
-    {start, stop}
-  end
-
-  defp expand_date(%{"year"=>year, "month"=>month, "day"=>day}) do
-    start = Timex.to_datetime({{String.to_integer(year), String.to_integer(month), String.to_integer(day)},{0,0,0}}, "Etc/UTC")
-    stop = Timex.end_of_day(start)
-    {start, stop}
-  end
-
-  defp proc_date(date) do ### do this better TODO
-    {:ok, value, i} = DateTime.from_iso8601(date <> ":00Z")
-    value
-  end
-
-  defp val_to_dates(%{"value" => "today", "value2" => ""}) do
-    start = Timex.now() |> Timex.beginning_of_day()
-    {start, Timex.end_of_day(start)}
-  end
-  defp val_to_dates(%{"value" => "tomorrow", "value2" => ""}) do
-    start = Timex.now() |> Timex.shift(days: 1) |> Timex.beginning_of_day()
-    {start, Timex.end_of_day(start)}
-  end
-  ### TODO more of these....
-
-  defp val_to_dates(%{"value" => v1, "value2" => ""}) do
-    Regex.named_captures(~r/(?<year>\d{4})-(?<month>\d{2})-?(?<day>\d{2})?/, v1) |> expand_date()
-  end
-
-  defp val_to_dates(%{"value" => v1, "value2" => v2} = f) do
-    IO.inspect(f)
-    ### Between
-    {proc_date(v1), proc_date(v2)}
-  end
-
   defp _make_date_filter(filter) do
-    IO.inspect(filter)
-    #
-    {start, stop} = val_to_dates(filter)
+    {start, stop} = Selecto.Helpers.Date.val_to_dates(filter)
     {:between, start, stop}
   end
 
