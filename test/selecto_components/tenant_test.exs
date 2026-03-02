62fdf3ab9993fb9e70c1ae1950f65e3bad380f8c
Chris Rohlfs <seeken@gmail.com>
2026-03-02 08:34:17 -0700

    test: expand tenant key isolation coverage

--- test/selecto_components/tenant_test.exs
+++ test/selecto_components/tenant_test.exs
@@ -11,4 +11,18 @@ defmodule SelectoComponents.TenantTest do
 
     assert scoped == "org:acme:/pagila"
   end
+
+  test "scoped_context isolates persisted keys between tenants" do
+    tenant_a = SelectoComponents.Tenant.scoped_context("/pagila", %{tenant_id: "acme"})
+    tenant_b = SelectoComponents.Tenant.scoped_context("/pagila", %{tenant_id: "globex"})
+
+    assert tenant_a == "tenant:acme:/pagila"
+    assert tenant_b == "tenant:globex:/pagila"
+    refute tenant_a == tenant_b
+  end
+
+  test "scoped_context supports atom contexts used in adapter modules" do
+    scoped = SelectoComponents.Tenant.scoped_context(:pagila_films, %{tenant_id: "acme"})
+    assert scoped == "tenant:acme:pagila_films"
+  end
 end
