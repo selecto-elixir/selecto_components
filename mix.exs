874d2f0caa42bd9abe240a4bf39afc3e079c2693
Chris Rohlfs <seeken@gmail.com>
2026-03-12 09:38:19 -0600

    chore: raise minimum Elixir version to 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SelectoComponents.MixProject do
     [
       app: :selecto_components,
       version: "0.3.20",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       description:
         "Alpha: Phoenix LiveView components for interactive Selecto query building and data exploration",
